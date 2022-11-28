deb1e89e85cd1c2fd9180744ea70949946dbfd85
Brian <bahrens96@gmail.com>
2022-11-28 17:16:24 -0500

    "Imported libaries and loaded images in"

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
 #Car Racing Game
 #Inspired by Tech with Tim | https://www.youtube.com/watch?v=L3ktUWfAMPg
 #11/28/22
+#On Part 1 @
+
+import pygame
+import time
+import math
+
+#load the images in
+GRASS = pygame.image.load("images/lawn.png")
+TRACK = pygame.image.load("images/track.png")
+
+TRACK_BORDER = pygame.image.load("images/track_outline.png")
+FINISH = pygame.image.load("images/finish_line.png")
+
+RED_CAR = pygame.image.load("images/red_car.png")
+GREEN_CAR = pygame.image.load("images/green_car.png")
 
