75da06da7112278dfbec37f86f14dbe708771486
Brian <bahrens96@gmail.com>
2022-12-01 17:21:10 -0500

    "Added a picture first version of the game board" "Added the keys to move the car, added the movement of the car" "Added the slow down function of the car" "Added a picture of the yucky trig behind the angle of the movement  of the car"

--- main.py
+++ main.py
@@ -30,11 +30,12 @@ FPS = 60
 class Abstract_Car:
     def __init__(self,max_vel,rotation_vel):
         self.max_vel = max_vel
-        self.vel = 0
+        self.vel = 0 #determines the speed of the car
         self.rotation_vel = rotation_vel
         self.angle = 0
         self.img = self.IMG
         self.x, self.y = self.START_POS
+        self.acceleration = 0.1
     
     def rotate(self,left=False, right=False):
         if left:
@@ -44,6 +45,24 @@ class Abstract_Car:
 
     def draw(self,win):
         blit_rotate_center(win,self.img,(self.x,self.y),self.angle)
+
+    def move_foward(self):
+        self.vel = min(self.vel + self.acceleration, self.max_vel) # if the accerlation is greater than the max vel, it defaults to the max vel
+        self.move()
+
+    def move(self):
+        radians = math.radians(self.angle)
+        #check yucky trig pic
+        vertical = math.cos(radians) * self.vel
+        horizontal = math.sin(radians) * self.vel
+
+        #plus returns positive values, makes car go foward, minus returns negative values, makes car go backwards
+        self.y += vertical
+        self.x += horizontal
+
+    def reduce_speed(self):
+        self.vel = max(self.vel-self.acceleration /2,0) #if the value is negative we dont wanna go backwards
+        self.move()
 
 class Player_Car(Abstract_Car):
     IMG = RED_CAR
@@ -72,5 +91,21 @@ while run:
         if event.type == pygame.QUIT:
             run = False
             break
+
+    keys = pygame.key.get_pressed()
+    moved = False
+
+    # rotating the car keys
+    if keys[pygame.K_a]:
+        player_car.rotate(left = True)
+    if keys[pygame.K_d]:
+        player_car.rotate(right = True)
+    if keys[pygame.K_w]:
+        moved = True
+        player_car.move_foward()
+
+    if not moved:
+        player_car.reduce_speed()
+
     
 pygame.quit()   
